baf0f3cb74b230d716b1b59e97350ff7adc26b14
Jack <ajackofsometrades@gmail.com>
2018-11-16 12:03:31 -0800

    basic app

---
+++ src/components/app/App.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { ROUTES } from '../../routes/routes';
+
+import Header from '../header/Header';
+import Footer from '../footer/Footer';
+
+import styles from './App.css';
+
+export default function App() {
+
+  return (
+    <Router>
+      <main className={styles.main}>
+        <Header />
+        <Switch>
+          <Route component={ROUTES.ABOUT.Component} path={ROUTES.ABOUT.path} />
+          <Route component={ROUTES.SEARCH.Component} path={ROUTES.SEARCH.path} />
+          <Route component={ROUTES.ARTIST.Component} path={ROUTES.ARTIST.path} />
+          <Redirect to={ROUTES.SEARCH.path} />
+        </Switch>
+        <Footer />
+      </main>
+    </Router>
+  );
+}
