439c8876a398b4b90331a5624dfa20ceaa571b57
Alex Rankin <alex.c.rankin@gmail.com>
2018-11-15 18:29:58 -0800

    initial

---
+++ src/components/app/App.js
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { ROUTES } from '../../routes/routes';
+
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <header>!header!</header>
+        <Switch>
+
+        </Switch>
+      </div>
+    </Router>
+
+  );
+}
---
+++ src/routes/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Search from '../components/search/Search';
+import ArtistDetail from '../components/artist/ArtistDetail';
+
+export const ROUTES = {
+  SEARCH: {
+    path: '/search',
+    Component: Search,
+    linkTo: () => '/search'
+  },
+  ARTIST: {
+    path: '/artists/:id',
+    Component: ArtistDetail,
+    linkTo: id => `/artists/${id}`
+  }
+};
