9d63d2a4fcb80410d14cc878a09fb3f2f701b618
KLPainter <KLPainter@gmail.com>
2018-11-19 12:09:21 -0800

    added app tests

---
+++ src/components/app/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from './App';
+import render from 'react-test-renderer';
+
+describe('App component tests', () => {
+  test('header snapshot', () => {
+    expect(render
+      .create(<App />)
+      .toJSON()).toMatchSnapshot();
+  });
+
+});
