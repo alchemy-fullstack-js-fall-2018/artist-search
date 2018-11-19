88432bdba7bf873ec49b403f03dbfb09cc195683
Davidchhing <davidhchhing@gmail.com>
2018-11-19 12:20:17 -0800

    Added paging functionality to move backwards and forwards through artist

--- src/components/artist/Artist.js
+++ src/components/artist/Artist.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '../../routes/routes';
 
+
 export default function Arist ({ name, description, id }) {
+
   return (
     <div>
       <h3>{name} - {description} <Link to={ROUTES.ARTIST.linkTo(id)}>Get Details: </Link> </h3>
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -6,6 +6,8 @@ import { getArtists } from '../../services/musicSearch';
 import { ROUTES } from '../../routes/routes';
 import Artist from '../artist/Artist';
 import styles from './search.css';
+import Paging from '../paging/Paging';
+
 
 class Search extends Component {
   static propTypes = {
@@ -14,10 +16,14 @@ class Search extends Component {
   };
 
   state = {
-    page: 1,
     totalPages: null,
     artists: []
   };
+
+    // state = {
+  //   currentPage: 1,
+  //   totalPages: '',
+  // }
 
   // shouldComponentUpdate(nextProps, nextState) {
   //   //PureComponent
@@ -28,16 +34,20 @@ class Search extends Component {
   // }
 
   doSearch = () => {
-    const { searchTerm } = queryString.parse(this.props.location.search.slice(1));
+    const { searchTerm, page } = queryString.parse(this.props.location.search.slice(1));
     if(!searchTerm) return;
 
-    getArtists(searchTerm)
-      .then(({ pages, artists }) => this.setState({ artists }));
+    getArtists(searchTerm, page)
+      .then(({ pages, artists }) => this.setState({ artists, totalPages: pages }));
   };
 
   componentDidMount() {
     this.doSearch();
   }
+
+  handlePageUpdate = page => {
+    this.updateHistory({ page });
+  };
 
   componentDidUpdate(prevProps, prevState) {
     // if(prevProps.location.search !== this.props.location.search) {
@@ -53,15 +63,24 @@ class Search extends Component {
   updateSearchTerm = event => {
     event.preventDefault();
     const searchTerm = document.getElementById('searchTerm').value;
-    const searchQuery = queryString.stringify({ searchTerm });
+    this.updateHistory({ searchTerm, page: 1 });
+  };
+
+updateHistory = search => {
+  const { searchTerm: currentSearchTerm, page: currentPage } = queryString.parse(this.props.location.search.slice(1));
+  const searchTerm = search.searchTerm || currentSearchTerm;
+  const page = search.page || currentPage;
+  const searchQuery = queryString.stringify({ searchTerm, page });
     this.props.history.push({
       pathName: ROUTES.SEARCH.path,
       search: searchQuery
     });
-  };
+}
 
   render() {
-    const artists = this.state.artists.map(artist => {
+    const { totalPages  } = this.state;
+    const { page } = queryString.parse(this.props.location.search.slice(1));
+    const artistsComponent = this.state.artists.map(artist => {
       return (
         <li key={artist.id}>
           <Artist id={artist.id}
@@ -73,13 +92,18 @@ class Search extends Component {
 
     return (
       <Fragment>
+
+        <Paging currentPage={page}
+          totalPages={totalPages}
+          updatePage={this.handlePageUpdate} />
         <Link to={ROUTES.LANDING.linkTo()}>Link!</Link>
+
         <form onSubmit={this.updateSearchTerm}>
           <input id="searchTerm" type="text" />
           <button type="submit">Search</button>
         </form>
         <ul className={styles.search}>
-          {artists}
+          {artistsComponent}
         </ul>
       </Fragment >
     );
