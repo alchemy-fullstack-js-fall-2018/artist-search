cc96dd29a807f5809ead4f9daae3bd9528b49ce3
Davidchhing <davidhchhing@gmail.com>
2018-11-19 10:39:51 -0800

    added search and artist details

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import { Routes } from '../../routes/routes';
+import { ROUTES } from '../../routes/routes';
 import Artist from '../artist/ArtistDetail';
 
 export default function App() {
@@ -9,11 +9,11 @@ export default function App() {
       <div>
         <h1>Artist Info</h1>
         <Switch>
-          <Route component={ROUTES.SEARCH.Component} path={ROUTES.SEARCH.path} />
+          {/* <Route component={ROUTES.SEARCH.Component} path={ROUTES.SEARCH.path} /> */}
           <Route component={ROUTES.ARTIST.Component} path={ROUTES.ARTIST.path} />
-          <Route render={ROUTES.HI.Component} path={ROUTES.HI.path} />
+          {/* <Route render={ROUTES.HI.Component} path={ROUTES.HI.path} /> */}
           {/* <Redirect from="/oldPage" to="/newPage" /> */}
-          <Redirect to={ROUTES.SEARCH.path} />
+          {/* <Redirect to={ROUTES.SEARCH.path} /> */}
         </Switch>
       </div>
     </Router>
