456deeb35f349f1e1943b04b9ace7b06f96be422
Sarah Flynn <sarah.flynn@icloud.com>
2018-11-20 12:22:26 -0800

    basic required functionality present

---
+++ src/components/artistDetail/ArtistDetail.test.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import ArtistDetail from './ArtistDetail';
+// import { shallow } from 'enzyme';
+
+// describe('artistDetail', () => {
+//     let wrapper;
+
+//     beforeEach(() => {
+//         wrapper = shallow(<ArtistDetail />);
+//     });
+
+//     it('renders a artistDetail component', () => {
+//         expect(wrapper).toMatchSnapshot();
+//     });
+// });
