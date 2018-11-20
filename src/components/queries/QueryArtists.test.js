eabc6e58a648f57304aeabd324bcbd52fecf96db
KLPainter <KLPainter@gmail.com>
2018-11-20 14:25:18 -0800

    one more test passing

--- src/components/queries/QueryArtists.test.js
+++ src/components/queries/QueryArtists.test.js
@@ -5,14 +5,14 @@ import QueryArtists from './QueryArtists';
 describe ('tests for QueryArtists component', () => {
 
   it('renders an QueryArtists component', () => {
-    const result = {
-      name: 'Joni Mitchell',
-      type: 'Person',
-      description: 'American folk singer',
-      id: '12345'
-
+    const onQueryItemChangeFn = jest.fn();
+    const query = {
+      name: 'Joni Mitchell'
     };
-    const wrapper = shallow(<QueryArtists result={result}/>);
+    const wrapper = shallow(
+      <QueryArtists query={query}
+        onQueryItemChange={onQueryItemChangeFn}
+      />);
     expect(wrapper).toMatchSnapshot();
   });
 
