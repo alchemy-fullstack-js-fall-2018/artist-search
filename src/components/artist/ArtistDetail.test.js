8c1154bfbe74278bca31d6653c185ab9d2991ebf
Davidchhing <davidhchhing@gmail.com>
2018-11-21 15:17:54 -0800

    fixed travis error

--- src/components/artist/ArtistDetail.test.js
+++ src/components/artist/ArtistDetail.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import ArtistDetail from './ArtistDetail'
-import Artist from './Artist'
+import ArtistDetail from './ArtistDetail';
+import Artist from './Artist';
 import { MemoryRouter } from 'react-router-dom';
 
 describe('Arist Test', () => {
