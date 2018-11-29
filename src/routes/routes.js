51e42548cec29b4eda273b9379fff9dfc0c218b8
Sophie Graham <sophiergraham1@gmail.com>
2018-11-28 17:27:18 -0800

    added more to my artist file

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,12 @@
+import Search from '../components/search/Search';
 import ArtistList from '../components/artists/ArtistList';
 
 export const ROUTES = {
+  SEARCH: {
+    path: '/search',
+    Component: Search,
+    linkTo: () => '/search'
+  },
   ARTISTS: {
     path: '/artists',
     Component: ArtistList,
